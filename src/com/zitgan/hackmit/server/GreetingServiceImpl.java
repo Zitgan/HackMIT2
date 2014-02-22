7eb823bbc51c8ed18261955bed9fff23f1f124e2
Tim Cargan <timcargan@gmail.com>
2014-02-22 10:35:51 -0500

    OP

--- src/com/zitgan/hackmit/server/GreetingServiceImpl.java
+++ src/com/zitgan/hackmit/server/GreetingServiceImpl.java
@@ -17,7 +17,7 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 			// If the input is not valid, throw an IllegalArgumentException back to
 			// the client.
 			throw new IllegalArgumentException(
-					"Name must be at least 4 characters long");
+					"Nam e must be at least 4 characters long");
 		}
 
 		String serverInfo = getServletContext().getServerInfo();
